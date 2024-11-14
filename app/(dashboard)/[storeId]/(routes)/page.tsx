5a7bcabd74f2d85a7c1f98ca120fd4c4311a5db2
N-feng <308561157@qq.com>
2024-11-14 21:46:53 +0800

    fix🐛: delete api & fix dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -5,9 +5,6 @@ import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
-import { getSalesCount } from "@/actions/get-sales-count";
-import { getGraphRevenue } from "@/actions/get-graph-revenue";
-import { getStockCount } from "@/actions/get-stock-count";
 import { formatter } from "@/lib/utils";
 import { getTotalProducts } from "@/actions/get-total-products";
 import { getTotalSales } from "@/actions/get-total-sales";
@@ -25,70 +22,26 @@ interface DashboardPageProps {
 const DashboardPage: React.FC<DashboardPageProps> = async ({ 
   params
 }) => {
-  // const totalRevenue = await getTotalRevenue(params.storeId);
-  // const totalSales = await getTotalSales(params.storeId);
-  // const totalProducts = await getTotalProducts(params.storeId);
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const totalSales = await getTotalSales(params.storeId);
+  const totalProducts = await getTotalProducts(params.storeId);
 
-  // const monthlyGraphRevenue = await getGraphTotalRevenue(params.storeId);
-  // const revenueByOrderStatus = await getOrderStatusTotalRevenue(params.storeId);
-  // const revenueBycategory = await getOrderTotalRevenueByCategory(
-  //   params.storeId
-  // );
-  // const orderStatusTotalRevenue = await getOrderPaymentStatusTotalRevenue(
-  //   params.storeId
-  // );
-
-  // const totalRevenue = await getTotalRevenue(params.storeId);
-  // const graphRevenue = await getGraphRevenue(params.storeId);
-  // const salesCount = await getSalesCount(params.storeId);
-  // const stockCount = await getStockCount(params.storeId);
+  const monthlyGraphRevenue = await getGraphTotalRevenue(params.storeId);
+  const revenueByOrderStatus = await getOrderStatusTotalRevenue(params.storeId);
+  const revenueBycategory = await getOrderTotalRevenueByCategory(
+    params.storeId
+  );
+  const orderStatusTotalRevenue = await getOrderPaymentStatusTotalRevenue(
+    params.storeId
+  );
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <Heading title="Dashboard" description="Overview of your store" />
         <Separator />
-        {/* <div className="grid gap-4 grid-cols-3">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Total Revenue
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{formatter.format(totalRevenue)}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Sales</CardTitle>
-              <CreditCard className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">+{salesCount}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Products In Stock</CardTitle>
-              <Package className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{stockCount}</div>
-            </CardContent>
-          </Card>
-        </div>
-        <Card className="col-span-4">
-          <CardHeader>
-            <CardTitle>Overview</CardTitle>
-          </CardHeader>
-          <CardContent className="pl-2">
-            <Overview data={graphRevenue} />
-          </CardContent>
-        </Card> */}
         <div className="grid gap-4 grid-cols-4">
-          {/* <Card className="col-span-2">
+          <Card className="col-span-2">
             <CardHeader className="flex items-center justify-between flex-row">
               <CardTitle className="text-sm font-medium">
                 Total Revenue
@@ -120,9 +73,9 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
             <CardContent>
               <div className="text-2xl font-bold">+{totalProducts}</div>
             </CardContent>
-          </Card> */}
+          </Card>
 
-          {/* <Card className="col-span-3">
+          <Card className="col-span-3">
             <CardHeader className="flex items-center justify-between flex-row">
               <CardTitle className="text-sm font-medium">
                 Revenue By Month
@@ -168,7 +121,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
             <CardContent>
               <Overview data={revenueByOrderStatus} />
             </CardContent>
-          </Card> */}
+          </Card>
         </div>
       </div>
     </div>
